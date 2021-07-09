891086e3dd51dc987744c5809740c175173627f7
Herman461 <zhukovskiyherman@gmail.com>
2021-07-09 10:23:06 +0300

    Урок 42. Внедрил настоящий redux.

--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -1,6 +1,21 @@
 import { SEND_MESSAGE, UPDATE_NEW_MESSAGE_TEXT } from './actions';
 
-export const dialogsReducer = (state, action) => {
+const initialState = {
+	dialogs: [
+	{ id: 1, name: 'Джон' },
+	{ id: 2, name: 'Иван' },
+	{ id: 3, name: 'Лера' },
+	{ id: 4, name: 'Катя' },
+	{ id: 5, name: 'Степан' },
+	],
+	messages: [
+	{ id: 1, text: 'Привет React' },
+	{ id: 2, text: 'Изучаю Реакт и Редакс!' },
+	{ id: 3, text: 'Мне нравится изучать JS!!!' },
+	],
+	newMessageText: '',
+}
+export const dialogsReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case SEND_MESSAGE:
 		let newMessage = { id: 4, text: state.newMessageText };
@@ -9,6 +24,8 @@ export const dialogsReducer = (state, action) => {
 		return state;
 		case UPDATE_NEW_MESSAGE_TEXT:
 		state.newMessageText = action.messageText;
+		return state;
+		default:
 		return state;
 	}
 }
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,6 +1,16 @@
 import { ADD_POST, UPDATE_NEW_POST_TEXT } from './actions';
 
-export const profileReducer = (state, action) => {
+const initialState = {
+	posts: [
+	{ likesCount: 23, message: 'Классная аватарка! Я на тебя подписан!', id: 1 },
+	{ likesCount: 5, message: 'Yo Yo Yo', id: 2 },
+	{ likesCount: 34, message: 'Хорошая веб-страница! Мне очень нравится', id: 3 },
+	{ likesCount: 120, message: 'Изучению программирование каждый день.', id: 4 },
+	],
+	newPostText: '',
+}
+
+export const profileReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case ADD_POST:
 		let newPost = { likesCount: 0, message: state.newPostText, id: 5 };
